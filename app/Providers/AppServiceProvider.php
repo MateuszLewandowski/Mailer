472090911ac29bad0a74a05f46906f9dc5f9608e
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-04 21:46:49 +0200

    soliD interfaces & mail alwaysTo admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,25 @@
 
 namespace App\Providers;
 
+use App\Interfaces\CaptchaInterface;
+use App\Interfaces\EmailInterface;
+use App\Services\CaptchaService;
+use App\Services\EmailService;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     * @todo Strategy pattern for the services via config file.
      *
      * @return void
      */
     public function register()
     {
-        //
+        $this->app->bind(EmailInterface::class, fn () => new EmailService(new CaptchaService));
+        $this->app->bind(CaptchaInterface::class, fn () => new CaptchaService);
     }
 
     /**
@@ -23,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('local')) {
+            Mail::alwaysTo('lewyy2501@gmail.com');
+        }
     }
 }
