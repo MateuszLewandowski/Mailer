45b392b6c709a1e6b5bd18e85a7caf49e6470ade
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-04 21:44:23 +0200

    configs

--- config/api.php
+++ config/api.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'token' => '',
+    'token' => '$2a$10$Bu0qajBtH4NURD6a3Sh/zuBa1auPdE3X62.FUjWRS1lCPr0lDz/Aq',
 ];
---
+++ config/microservice.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'store_meta' => true,
+
+    /**
+     * 1 => 'ADDITION',
+     * 2 => 'SUBTRACTION',
+     * 3 => 'DIVISION',
+     * 4 => 'MULTIPLICATION',
+     */
+    'default_captcha' => 'addition',
+];
