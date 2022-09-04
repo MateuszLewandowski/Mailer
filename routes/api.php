e883c4ce717ef8bc81244d446f7e748df7a31162
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-04 21:47:01 +0200

    Routes

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ApiHealthController;
+use App\Http\Controllers\CaptchaController;
+use App\Http\Controllers\EmailController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group([
+    'middleware' => ['api'],
+], function() {
+    Route::get('health', [ApiHealthController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::prefix('email')->group(function() {
+        Route::get('initialize', [EmailController::class, 'initialize'])->name('send.initialize');
+    });
+
+    Route::middleware(['authorized'])->group(function() {
+        Route::prefix('email')->group(function() {
+            Route::post('send', [EmailController::class, 'send'])->name('send.email');
+        });
+        Route::prefix('captcha')->group(function() {
+            Route::get('refresh', [CaptchaController::class, 'refresh'])->name('captcha.refresh');
+        });
+    });
 });
 
+Route::fallback(fn () => response('Not found.', 404));
