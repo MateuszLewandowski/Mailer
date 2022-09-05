1e6aaf3c902f79f8ef4f6485006a47a674a574ef
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-05 19:19:08 +0200

    Refresh captcha

--- app/Http/Controllers/CaptchaController.php
+++ app/Http/Controllers/CaptchaController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RefreshCaptchaRequest;
+use App\Interfaces\CaptchaInterface;
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class CaptchaController extends Controller
 {
+    public function __construct(
+        private CaptchaInterface $captchaService,
+    ) {
+    }
+
     public function refresh(RefreshCaptchaRequest $request): JsonResponse
     {
-
+        return response()->apiResponse(
+            status: Response::HTTP_RESET_CONTENT,
+            message: null,
+            data: [
+                'captcha' => $this->captchaService->refresh(id: $request->get('pending_request')['captcha_id']),
+            ],
+        );
     }
 }
--- app/Services/CaptchaService.php
+++ app/Services/CaptchaService.php
@@ -4,8 +4,6 @@ namespace App\Services;
 
 use App\Interfaces\CaptchaInterface;
 use App\Models\Captcha;
-use App\Models\PendingRequest;
-use Illuminate\Support\Facades\DB;
 use Throwable;
 
 final class CaptchaService implements CaptchaInterface
@@ -16,6 +14,7 @@ final class CaptchaService implements CaptchaInterface
         3 => 'DIVISION',
         4 => 'MULTIPLICATION',
     ];
+
     private int $operation_id;
 
     public function __construct(
@@ -33,6 +32,7 @@ final class CaptchaService implements CaptchaInterface
                 'y' => $y,
                 'result' => $result,
             ]);
+
             return [
                 'operation' => $this->operations[$this->operation_id],
                 'x' => $x,
@@ -44,23 +44,18 @@ final class CaptchaService implements CaptchaInterface
         }
     }
 
-    public function refresh(string $uuid): array
+    public function refresh(int $id): array
     {
         try {
-            $captcha = DB::table('requests')
-                ->selectRaw('requests.id as request_id, captcha.*')
-                ->join('captcha', 'requests.id', '=', 'captcha.request_id')
-                ->where('requests.uuid', '=', $uuid);
             [$x, $y, $result] = self::getMathIngredients();
-            $result = DB::table('captcha')
-                ->where('id', '=', $captcha->id)
+            Captcha::where('id', '=', $id)
                 ->update([
-                    'request_id' => $captcha->request_id,
                     'operation_id' => $this->operation_id,
                     'x' => $x,
                     'y' => $y,
                     'result' => $result,
                 ]);
+
             return [
                 'operation' => $this->operations[$this->operation_id],
                 'x' => $x,
@@ -72,76 +67,76 @@ final class CaptchaService implements CaptchaInterface
         }
     }
 
-    public static function verify(int $obtained, int $expected): bool {
+    public static function verify(int $obtained, int $expected): bool
+    {
         return $obtained === $expected;
-    }
-
-    private static function getAnswerByUUID(string $uuid) {
-        try {
-            return DB::table('request')
-                ->select('captcha.result')
-                ->rightJoin('captcha', 'request.id', '=', 'captcha.request_id')
-                ->where('request.uuid', '=', $uuid)
-                ->firstOrFail();
-        } catch (Throwable $e) {
-            throw $e;
-        }
     }
 
     private function getMathIngredients(): array
     {
         $this->operation_id = array_rand($this->operations);
         $operation = $this->operations[$this->operation_id];
-        switch ($operation)
-        {
+        switch ($operation) {
             case 'SUBTRACTION':
                 $x = self::getLargeEasyNumber();
                 $y = self::getSmallNumber();
+
                 return [$x, $y, $x - $y];
             case 'MULTIPLICATION':
                 $x = self::getVerySmallNumber();
                 $y = self::getVerySmallNumber();
+
                 return [$x, $y, $x * $y];
             case 'DIVISION':
                 [$x, $y] = self::getDivisionNumberPairs();
+
                 return [$x, $y, $x / $y];
             case 'ADDITION':
             default:
                 $x = self::getLargeEasyNumber();
                 $y = self::getSmallNumber();
+
                 return [$x, $y, $x + $y];
                 break;
         }
     }
 
-    private static function getLargeEasyNumber(): int {
+    private static function getLargeEasyNumber(): int
+    {
         $x = mt_rand(3, 7);
         $y = mt_rand(1, 4);
-        return (int) $x . $y;
+
+        return (int) $x.$y;
     }
 
-    private static function getSmallNumber(): int {
+    private static function getSmallNumber(): int
+    {
         return mt_rand(1, 6);
     }
 
-    private static function getVerySmallNumber(): int {
+    private static function getVerySmallNumber(): int
+    {
         return mt_rand(1, 4);
     }
 
-    private static function getDivisionNumberPairs(): array {
+    private static function getDivisionNumberPairs(): array
+    {
         $x = self::getSmallEvenNumber();
         $y = self::getVerySmallEvenNumber();
         if ($x % $y !== 0) {
             self::getDivisionNumberPairs();
         }
+
         return [$x, $y];
     }
 
-    private static function getSmallEvenNumber(): int {
+    private static function getSmallEvenNumber(): int
+    {
         return array_rand(array_flip([6, 8, 10, 12, 14, 16]));
     }
 
-    private static function getVerySmallEvenNumber(): int {
+    private static function getVerySmallEvenNumber(): int
+    {
         return array_rand(array_flip([2, 4]));
     }
 }
