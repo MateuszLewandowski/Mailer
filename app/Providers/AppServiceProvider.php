9a9d8d03c6872abaaac4ffe58a8d16ebfc5e8618
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-05 19:21:44 +0200

    Meta&Request services through interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,12 @@ namespace App\Providers;
 
 use App\Interfaces\CaptchaInterface;
 use App\Interfaces\EmailInterface;
+use App\Interfaces\MetaInterface;
+use App\Interfaces\RequestInterface;
 use App\Services\CaptchaService;
 use App\Services\EmailService;
+use App\Services\MetaService;
+use App\Services\RequestService;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
@@ -13,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
      * @todo Strategy pattern for the services via config file.
      *
      * @return void
@@ -21,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(EmailInterface::class, fn () => new EmailService(new CaptchaService));
         $this->app->bind(CaptchaInterface::class, fn () => new CaptchaService);
+        $this->app->bind(MetaInterface::class, fn () => new MetaService);
+        $this->app->bind(RequestInterface::class, fn () => new RequestService);
     }
 
     /**
