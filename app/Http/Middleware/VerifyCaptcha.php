e8dcca718b951df75811b2d9542fd5205d262d87
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-05 19:16:01 +0200

    Excluded url for refreshing captcha

--- app/Http/Middleware/VerifyCaptcha.php
+++ app/Http/Middleware/VerifyCaptcha.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use App\Models\Captcha as CaptchaModel;
-use App\Models\Request as RequestModel;
 use App\Services\CaptchaService;
+use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -13,11 +11,14 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class VerifyCaptcha
 {
     private array $excluded = [
-        '/api/sm/captcha/refresh'
+        '/api/ms/captcha/refresh',
     ];
+
     /**
      * Handle an incoming request.
+     *
      * @todo HttpException.
+     *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
@@ -27,9 +28,10 @@ class VerifyCaptcha
         if (in_array($request->getRequestUri(), $this->excluded)) {
             return $next($request);
         }
-        if (!$captcha = $request->get('captcha', false)) {
+        if (! $captcha = $request->get('captcha', false)) {
             throw new HttpException(Response::HTTP_UNPROCESSABLE_ENTITY);
         }
+
         return CaptchaService::verify(obtained: $captcha, expected: $request->pending_request['result'])
             ? $next($request)
             : throw new HttpException(Response::HTTP_UNPROCESSABLE_ENTITY);
